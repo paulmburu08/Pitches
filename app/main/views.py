92e09fe68d7e727f53a536229364f8d85c64ad53
paulmburu08 <paulmburu08@gmail.com>
2020-06-08 20:21:04 +0300

    add navbar

--- app/main/views.py
+++ app/main/views.py
@@ -2,4 +2,11 @@ from flask import render_template
 from . import main
 
 @main.route('/')
-def index
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'PITCHES'
+
+    return render_template('index.html', title = title)
