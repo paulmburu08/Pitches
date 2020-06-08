74648f07ba5b5236a6a4e7854afbae92fee3debb
paulmburu08 <paulmburu08@gmail.com>
2020-06-08 22:59:24 +0300

    add migrations folder

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
+from flask_migrate import Migrate, MigrateCommand
 from app.models import User,Role
 
 # Creating app instance
@@ -7,6 +8,10 @@ app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
 @manager.command
 def test():
     """Run the unit tests."""
