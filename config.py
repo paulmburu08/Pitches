6eb60b6361ed02c49fd4c58d59a6c06b80057094
paulmburu08 <paulmburu08@gmail.com>
2020-06-08 16:30:53 +0300

    add config

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+class Config:
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
