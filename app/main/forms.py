281029999ec1edbb079003a2afed12e83cc471a5
paulmburu08 <paulmburu08@gmail.com>
2020-06-09 18:14:45 +0300

    add changes

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField
+from wtforms import TextAreaField,SubmitField
+from wtforms.validators import Required
 
 class AddComment(FlaskForm):
 
     comment = TextAreaField('Comment')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,9 @@
-from flask import render_template
+from flask import render_template,abort,redirect,url_for
 from . import main
 from flask_login import login_required
-from .forms import AddComment
+from .forms import AddComment,UpdateProfile
+from ..models import User
+from .. import db
 
 @main.route('/')
 def index():
@@ -21,3 +23,22 @@ def profile(uname):
         abort(404)
 
     return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
