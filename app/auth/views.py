fe3cbdc2070c6aa8223c8d3150cf5e3167b2beb9
paulmburu08 <paulmburu08@gmail.com>
2020-06-09 18:04:44 +0300

    add bio and p.pic path

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 from flask import render_template,redirect,url_for,flash,request
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required
 from ..models import User
 from .forms import RegistrationForm,LoginForm
 from . import auth
@@ -29,3 +29,9 @@ def login():
 
     title = "Pitches login"
     return render_template('auth/login.html',login_form = login_form,title=title)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
--- app/main/views.py
+++ app/main/views.py
@@ -12,3 +12,12 @@ def index():
     title = 'PITCHES'
 
     return render_template('index.html', title = title)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
