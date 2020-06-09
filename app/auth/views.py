20ad73be74aab69b21441b25fa64eb786dc38633
paulmburu08 <paulmburu08@gmail.com>
2020-06-09 16:29:49 +0300

    add login route and form

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,7 @@
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for,flash,request
+from flask_login import login_user
 from ..models import User
-from .forms import RegistrationForm
+from .forms import RegistrationForm,LoginForm
 from . import auth
 from .. import db
 
@@ -14,3 +15,17 @@ def register():
         db.session.commit()
         return redirect(url_for('auth.login'))
     return render_template('auth/register.html',registration_form = form, title = title)
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "Pitches login"
+    return render_template('auth/login.html',login_form = login_form,title=title)
