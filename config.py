e389d2ef9f8339da6cc4b752632da70eb2877198
paulmburu08 <paulmburu08@gmail.com>
2020-06-10 18:04:53 +0300

    add changes

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://paul:pauldatabase@localhost/pitches'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     #  email configurations
